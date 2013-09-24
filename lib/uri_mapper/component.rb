620500548ab169b8d880900cf985bc9b9372c57d
Andrew Radev <andrey.radev@gmail.com>
2013-09-24 21:36:16 +0200

    Some documentation

--- lib/uri_mapper/component.rb
+++ lib/uri_mapper/component.rb
@@ -1,4 +1,8 @@
 module UriMapper
+  # The skeleton for a URI component, like Query, Path, etc. The underlying
+  # implementations can vary, but this class provides the minimal protocol they
+  # need to guarantee so the Uri can use them.
+  #
   class Component
     def self.build(source)
       if source.is_a?(self)
@@ -8,14 +12,39 @@ module UriMapper
       end
     end
 
+    # The constructor of the Component is called when a component is first
+    # accessed by the Uri. It is then initialized with some particular
+    # properties of the underlying URI object that are defined by the builder.
+    #
+    # This may overlap with #reload, but it's not necessarily the case.
+    #
     def initialize(*)
       raise NotImplementedError
     end
 
+    # Reloading a component means providing it with the necessary data for it
+    # to change its contents.
+    #
+    # The difference between #reload and #initialize is that this method will
+    # not be called by the Uri upon construction, but upon setting by the user,
+    # so it's possible that this method will have different concerns than
+    # #initialize.
+    #
+    # For instance, compare:
+    #
+    #   Query.new('some=query&string')
+    #   query.reload(:different => 'format')
+    #
+    # In practice, it's likely that it will overlap with #initialize, though.
+    #
     def reload(*)
       raise NotImplementedError
     end
 
+    # The serialize method is the one that will be used once this component is
+    # assigned to the core uri. This tends to be somewhat inconsistent, so it
+    # can't be relied only on #to_s to provide a simple serializer.
+    #
     def serialize
       raise NotImplementedError
     end
@@ -25,6 +54,13 @@ module UriMapper
     end
     alias_method :+, :merge
 
+    # Combines the Component with another component. It's up to the
+    # implementation to decide how exactly to treat it, but this should result
+    # in an aggregation of the contents of both components.
+    #
+    # The `other` attribute may not be a Component itself, it's recommended to
+    # trigger Component::build on it before working with it.
+    #
     def merge!(other)
       raise NotImplementedError
     end
